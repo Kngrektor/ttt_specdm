a46f9e7f20e682ed1aa8d46588ebe4b95327ff81
Kng <kngrektor@gmail.com>
2020-07-20 22:41:17 +0200

    new directory structure, cuz I can

---
+++ lua/autorun/ttt_specdm.lua
@@ -0,0 +1,21 @@
+SpecDM = SpecDM or {}
+
+local CL = SERVER and AddCSLuaFile or include
+local SV = SERVER and include or function() end
+local function SH(file) CL(file) SV(file) end
+
+SH("libs/von.lua")
+SH("specdm/config.lua")
+
+CL("specdm/cl_specdm.lua")
+SV("specdm/sv_specdm.lua")
+SH("specdm/sh_specdm.lua")
+
+CL("specdm/cl_hud.lua")
+CL("vgui/spec_dm_loadout.lua")
+CL("specdm/cl_stats.lua")
+
+SV("specdm/sv_overrides.lua")
+SV("specdm/sv_stats.lua")
+
+SH("specdm/sh_announcer.lua")
